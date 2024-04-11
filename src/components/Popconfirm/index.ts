b5c6620dc82f8604643fb17601ab57684d1b3633
Ayechanpaing31 <116068529+Ayechanpaing31@users.noreply.github.com>
2024-04-11 22:47:32 +0800

    test

--- src/components/Popconfirm/index.ts
+++ src/components/Popconfirm/index.ts
@@ -1,1 +1,28 @@
-export { default } from './Popconfirm';
+export {
+    createPopoverScope,
+    //
+    Popover,
+    PopoverAnchor,
+    PopoverTrigger,
+    PopoverPortal,
+    PopoverContent,
+    PopoverClose,
+    PopoverArrow,
+    //
+    Root,
+    Anchor,
+    Trigger,
+    Portal,
+    Content,
+    Close,
+    Arrow,
+  } from './Popconfirm';
+  export type {
+    PopoverProps,
+    PopoverAnchorProps,
+    PopoverTriggerProps,
+    PopoverPortalProps,
+    PopoverContentProps,
+    PopoverCloseProps,
+    PopoverArrowProps,
+  } from './Popconfirm';
