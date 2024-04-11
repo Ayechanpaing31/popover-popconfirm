8972936a3c4789d42671fbbcfb6fa6808acc17e4
Ayechanpaing31 <116068529+Ayechanpaing31@users.noreply.github.com>
2024-04-12 03:11:25 +0800

    fixinggg

--- rollup.config.js
+++ rollup.config.js
@@ -24,14 +24,12 @@ export default [
       resolve(),
       commonjs(),
       typescript({
-        tsconfig: './tsconfig.json',
-        declaration: true,
-        declarationDir: 'dist',
+        tsconfig: './tsconfig.json'
      })
     ],
   },
   {
-    input: "dist/esm/index.d.ts",
+    input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts.default()],
   }
