71e112827c39246869d4653d8731def10d6f618a
Ayechanpaing31 <116068529+Ayechanpaing31@users.noreply.github.com>
2024-04-15 05:18:15 +0800

    fix a few thingssss

--- src/components/Popconfirm/Popconfirm.tsx
+++ src/components/Popconfirm/Popconfirm.tsx
@@ -1,55 +1,18 @@
-// 'use client'
-
-// import * as React from 'react'
-// import * as PopoverPrimitive from '@radix-ui/react-popover'
-
-// import { cn } from '../../lib/utils'
-
-// const Popover = PopoverPrimitive.Root
-
-// const PopoverTrigger = PopoverPrimitive.Trigger
-
-// const PopoverContent = React.forwardRef<
-//   React.ElementRef<typeof PopoverPrimitive.Content>,
-//   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
-// >(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
-//   <PopoverPrimitive.Portal>
-//     <PopoverPrimitive.Content
-//       ref={ref}
-//       align={align}
-//       sideOffset={sideOffset}
-//       className={cn(
-//         'z-50 w-72 rounded-md border border-slate-100 bg-white p-4 shadow-md outline-none animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=top]:slide-in-from-bottom-2 dark:border-slate-800 dark:bg-slate-800',
-//         className
-//       )}
-//       {...props}
-//     >
-//      <PopoverPrimitive.Arrow offset={10} style={{ fill: 'white' }} />
-    
-//      <div>
-//         <button>Yes</button>
-//         <button>No</button>
-//       </div>
-//      </PopoverPrimitive.Content>
-//   </PopoverPrimitive.Portal>
-// ))
-// PopoverContent.displayName = PopoverPrimitive.Content.displayName
-
-// export { Popover, PopoverTrigger, PopoverContent }
-
 'use client'
 
-import * as React from 'react'
-import * as PopoverPrimitive from '@radix-ui/react-popover'
+import React from 'react';
+import * as PopoverPrimitive from '@radix-ui/react-popover';
 
 import { cn } from '../../lib/utils';
 import './Popconfirm.css';
 
-interface PopoverConfirmContentProps extends React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> {
+export interface PopoverConfirmContentProps extends React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> {
   confirmText: string;
   cancelText: string;
   onConfirm: () => void;
   onCancel: () => void;
+  title: string;
+  description: string;
 }
 
 const PopoverConfirm = PopoverPrimitive.Root
@@ -72,10 +35,13 @@ const PopoverConfirmContent = React.forwardRef<
       )}
       {...props}
     >
-      {children}
-      <div className={cn('flex justify-end mt-4 space-x-2')}>
+       <div>
+        <h2 className={cn('mt-2 mb-2 text-black font-bold')}>{props.title}</h2>
+        <p className={cn(' text-black')}>{props.description}</p>
+      </div>
+      <div className={cn('flex justify-end mt-1 space-x-2')}>
         <button onClick={props.onCancel} className={cn('text-black bg-transparent mr-4')}>{props.cancelText}</button>
-        <button onClick={props.onConfirm } className={cn('bg-purple-500 text-white mx-4')}>{props.confirmText}</button>
+        <button onClick={props.onConfirm } className={cn('bg-purple-500 text-white p-2')}>{props.confirmText}</button>
       </div>
       <PopoverPrimitive.Arrow offset={10} style={{ fill: 'white' }} />
     </PopoverPrimitive.Content>
--- src/components/Popconfirm/index.ts
+++ src/components/Popconfirm/index.ts
@@ -1,1 +1,2 @@
-export * from './Popconfirm';
+export { PopoverConfirm, PopoverConfirmTrigger, PopoverConfirmContent } from './Popconfirm';
+
